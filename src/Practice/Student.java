01b8a496ba075d69baef09a24006ea95be581448
Saveliev Mikhail <Savelievmv@gmail.com>
2016-11-26 20:42:38 +0100

    Class Student with methods

--- src/Practice/Student.java
+++ src/Practice/Student.java
@@ -4,6 +4,9 @@ package Practice;
  * Created by Михаил on 26.11.2016.
  */
 public class Student {
+    private String Name;
+    private int TotalScore;
+    private int numOfQuizzes;
 
 
     public Student(String nm) {
