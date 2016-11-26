b364e8a0e87f8851f5708dcad2a8df8c09bbe406
Saveliev Mikhail <Savelievmv@gmail.com>
2016-11-26 22:25:09 +0100

    Program works

---
+++ src/Practice/StudentTester.java
@@ -0,0 +1,19 @@
+package Practice;
+
+/**
+ * Created by Михаил on 26.11.2016.
+ */
+public class StudentTester {
+    public static void main(String[] args) {
+        Student Misha = new Student("A");
+        Misha.addGrade("A+");
+        Misha.addGrade("B");
+        Misha.addQuiz(6);
+        Misha.addQuiz(8);
+        System.out.println(Misha.getAverageScore());
+        System.out.println(Misha.getName());
+        System.out.println(Misha.getTotalScore());
+        System.out.println(Misha.getGPA());
+        System.out.println(Misha.getAverageScore());
+    }
+}
