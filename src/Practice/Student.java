faada0090ecd9b715b1b91cdad46f755089d1178
Saveliev Mikhail <Savelievmv@gmail.com>
2016-11-26 20:16:54 +0100

    Class Student with methods

---
+++ src/Practice/Student.java
@@ -0,0 +1,28 @@
+package Practice;
+
+/**
+ * Created by Михаил on 26.11.2016.
+ */
+public class Student {
+
+
+    public Student(String nm) {
+
+    }
+
+    public String getName(){
+
+    }
+
+    public void addQuiz(int score) {
+
+    }
+
+    public int getTotalScore() {
+
+    }
+
+    public double getAverageScore() {
+
+    }
+}
