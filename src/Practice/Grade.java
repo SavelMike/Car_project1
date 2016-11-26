98d3de7e1d404673abc17295c558459021a89f3f
Saveliev Mikhail <Savelievmv@gmail.com>
2016-11-26 22:03:49 +0100

    Class Grade with methods

--- src/Practice/Grade.java
+++ src/Practice/Grade.java
@@ -5,12 +5,55 @@ package Practice;
  */
 public class Grade {
 
+    private String strGrade;
+
 
     public Grade(String gr) {
-
+        strGrade = gr;
     }
 
-    public double gradeToPoints(){
-
+    public double gradeToPoints() {
+        if (strGrade.equals("A+")) {
+            return 4.0;
+        }
+        if (strGrade.equals("A")) {
+            return 4.0;
+        }
+        if (strGrade.equals("-A")) {
+            return 3.7;
+        }
+        if (strGrade.equals("B+")) {
+            return 3.3;
+        }
+        if (strGrade.equals("B")) {
+            return 3.0;
+        }
+        if (strGrade.equals("B-")) {
+            return 2.7;
+        }
+        if (strGrade.equals("C+")) {
+            return 2.3;
+        }
+        if (strGrade.equals("C")) {
+            return 2.0;
+        }
+        if (strGrade.equals("C-")) {
+            return 1.7;
+        }
+        if (strGrade.equals("D+")) {
+            return 1.3;
+        }
+        if (strGrade.equals("D")) {
+            return 1.0;
+        }
+        if (strGrade.equals("D-")) {
+            return 0.7;
+        }
+        if (strGrade.equals("F")) {
+            return 0.0;
+        }else {
+            System.exit(1);
+            return 0;
+        }
     }
 }
