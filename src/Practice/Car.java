e4b6e57ec69da06a05512c9cb970b070a744f662
Saveliev Mikhail <Savelievmv@gmail.com>
2016-11-26 18:41:34 +0100

    Initial commit

---
+++ src/Practice/Car.java
@@ -0,0 +1,24 @@
+package Practice;
+
+/**
+ * Created by Михаил on 26.11.2016.
+ */
+public class Car {
+
+
+    public Car() {
+
+    }
+
+    public void drive() {
+
+    }
+
+    public void getGasLevel() {
+
+    }
+
+    public void addGas() {
+
+    }
+}
