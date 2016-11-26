dd0b14bfc12d348adb6f8302412193aded3455c2
Saveliev Mikhail <Savelievmv@gmail.com>
2016-11-26 20:50:25 +0100

    Class Student with methods works

--- src/Practice/Student.java
+++ src/Practice/Student.java
@@ -10,22 +10,25 @@ public class Student {
 
 
     public Student(String nm) {
-
+        Name = nm;
+        TotalScore = 0;
+        numOfQuizzes = 0;
     }
 
-    public String getName(){
-
+    public String getName() {
+        return Name;
     }
 
     public void addQuiz(int score) {
-
+        TotalScore += score;
+        numOfQuizzes++;
     }
 
     public int getTotalScore() {
-
+        return TotalScore;
     }
 
     public double getAverageScore() {
-
+        return TotalScore / numOfQuizzes;
     }
 }
