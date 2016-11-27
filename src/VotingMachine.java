965d60e287f9a079ae77d283b49a3f9ae0f22912
Saveliev Mikhail <Savelievmv@gmail.com>
2016-11-27 21:05:27 +0100

    VotingMachine class, implementation of methods

--- src/VotingMachine.java
+++ src/VotingMachine.java
@@ -3,23 +3,32 @@
  */
 public class VotingMachine {
 
-    public VotingMachine() {
+    private int democratCount;
+    private int republicCount;
 
+    public VotingMachine() {
+        democratCount = 0;
+        republicCount = 0;
     }
 
     public void clearMachine() {
-
+        democratCount = 0;
+        republicCount = 0;
     }
 
     public void voteDemocrat() {
-
+        democratCount++;
     }
 
     public void voteRepublican() {
-
+        republicCount++;
     }
 
-    public int getTallies() {
+    public int getDemVotes() {
+        return democratCount;
+    }
 
+    public int getRepVotes() {
+        return republicCount;
     }
 }
