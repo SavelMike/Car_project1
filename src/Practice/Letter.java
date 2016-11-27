f7837774dfa1911c276cdef7e66b7fc6c5355406
Saveliev Mikhail <Savelievmv@gmail.com>
2016-11-27 21:19:42 +0100

    Class Letter, methods

---
+++ src/Practice/Letter.java
@@ -0,0 +1,27 @@
+package Practice;
+
+/**
+ * Created by Михаил on 27.11.2016.
+ */
+public class Letter {
+
+    private String sender;
+    private String recipient;
+    private String text;
+
+    public Letter(String from, String to){
+
+    }
+
+    public void addLine(String line) {
+
+    }
+
+    public String getText() {
+
+    }
+
+    public void main () {
+
+    }
+}
